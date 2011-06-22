0153613f41d4c95073b30205c5de5734caaf4838
Konstantin Dankov <konstantin.dankov@gmail.com>
2011-06-22 15:10:05 +0100

    Fix title

--- header.php
+++ header.php
@@ -13,12 +13,8 @@
 
 		// Add the blog description for the home/front page.
 		$site_description = get_bloginfo( 'description', 'display' );
-		if ( $site_description && ( is_home() || is_front_page() ) )
-			echo " | $site_description";
 
-		// Add a page number if necessary:
-		if ( $paged >= 2 || $page >= 2 )
-			echo ' | ' . sprintf( __( 'Page %s', 'twentyten' ), max( $paged, $page ) );
+		if ( $site_description && ( is_home() || is_front_page() ) ) echo " | $site_description";
 
 	?></title>
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
