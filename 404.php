99aaccadcc214fb0198803563f45c045d86d98cd
Konstantin Dankov <konstantin.dankov@gmail.com>
2011-06-23 19:55:16 +0100

    Added 404 Error page.

---
+++ 404.php
@@ -0,0 +1,23 @@
+<?php get_header(); ?>
+
+		<div id="container">
+			<div id="content" role="main">
+				<div class="entry">
+					<div class="entry-header">
+						<h1>Oops!</h1>
+						<h3>I'm Sorry. The page you are looking for doesn't exsist.</h3>
+					</div>
+					<div class="meta">&nbsp;</div>
+					<div class="entry-content">
+						<br>
+						<p>As unfotunate as this is you are not without options. You can go the <a href="/">home page</a> and see whats latest or visit the <a href="/about">About</a> page and read interesting stuff about this blog.</p>
+
+						<?php /* <p>If you know what are you looking for you can try and search for it.</p> */ ?>
+
+						<?php /* get_search_form(); */ ?>
+					</div>
+				</div>
+			</div>
+		</div>
+
+<?php get_footer(); ?>
