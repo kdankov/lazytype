23a1b40ef2b8a6368b5dad87a6d641437a4bc8d1
Konstantin Dankov <konstantin.dankov@gmail.com>
2011-11-24 09:36:12 +0000

    Meta attributes for mobile screens

--- header.php
+++ header.php
@@ -2,6 +2,8 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
 	<title><?php
 
 		global $page, $paged;
