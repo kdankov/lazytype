f9db51bc31e25ca40d4ee7325b6026f705b1a39c
Konstantin Dankov <konstantin.dankov@gmail.com>
2012-03-07 15:29:33 +0000

    Quotes styles

--- single-quotes.php
+++ single-quotes.php
@@ -4,23 +4,15 @@
 			<div id="content" role="main">
 
 				<?php while ( have_posts() ) : the_post(); ?>
-					<div class="entry">
+					<div class="quote">
 						<div class="entry-header">
 							<h1><?php the_title(); ?></h1>
 						</div>
-						<div class="meta">
-							<span class="day-of-the-week"><?php the_time('l'); ?>,</span> <span class="date"><?php the_time('F jS'); ?></span> <span class="time">at <?php the_time(); ?></span>
-						</div>
-						<div class="entry-content">
+						<div class="quote-content">
 							<?php the_content(); ?>
-						</div>
-						<div class="entry-footer">
-
 						</div>
 					</div>
 				<?php endwhile; ?>
-
-				<?php comments_template( '', true ); ?>
 
 			</div>
 		</div>
--- type-quotes.php
+++ type-quotes.php
@@ -1,34 +1,16 @@
 <?php get_header(); ?>
 
-	<?php if( is_home() && false ){ ?>
-		<div id="welcome">
-			<?php dynamic_sidebar( 'homepage' ); ?>
-		</div>
-	<?php } ?>
-
 	<div id="container">
 		<div id="content" role="main">
 
-			<?php if ( $wp_query->max_num_pages > 1 ) : ?>
-				<div id="nav-above" class="navigation">
-					<div class="nav-previous"><?php next_posts_link('<span class="meta-nav">&larr;</span> Older posts'); ?></div>
-					<div class="nav-next"><?php previous_posts_link('Newer posts <span class="meta-nav">&rarr;</span>'); ?></div>
-				</div>
-			<?php endif; ?>
-
+            <h1>Quotes</h1>
 			<?php while ( have_posts() ) : the_post(); ?>
-				<div class="entry">
+				<div class="quote">
 					<div class="entry-header">
 						<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
 					</div>
-					<div class="meta">
-						<span class="day-of-the-week"><?php the_time('l'); ?>,</span> <span class="date"><?php the_time('F jS'); ?></span> <span class="time">at <?php the_time(); ?></span>
-					</div>
-					<div class="entry-content">
-						<?php the_excerpt(); ?>
-					</div>
-					<div class="entry-footer">
-
+					<div class="quote-content">
+						<?php the_content(); ?>
 					</div>
 				</div>
 			<?php endwhile; ?>
