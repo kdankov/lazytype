520efa15cbe8f1ae1f6db55aef8d251f973f6f83
Konstantin Dankov <konstantin.dankov@gmail.com>
2012-03-14 15:46:10 +0000

    Year fix
    Copy change

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 					<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
 				</div>
 				<div class="meta">
-					<div class="inner"><span class="day-of-the-week"><?php the_time('l'); ?>,</span> <span class="date"><?php the_time('F jS'); ?></span> <span class="time">at <?php the_time(); ?></span></div>
+					<div class="inner"><span class="day-of-the-week"><?php the_time('l'); ?>,</span> <span class="date"><?php the_time('F jS Y'); ?></span></div>
 				</div>
 				<div class="entry-content">
 					<div class="inner"><?php the_excerpt(); ?></div>
@@ -25,8 +25,8 @@
 
 		<?php if (  $wp_query->max_num_pages > 1 ) : ?>
 			<div id="nav-below" class="navigation">
-				<div class="nav-previous"><?php next_posts_link( '<span class="meta-nav">&larr;</span> Older posts' ); ?></div>
-				<div class="nav-next"><?php previous_posts_link( 'Newer posts <span class="meta-nav">&rarr;</span>' ); ?></div>
+				<div class="nav-previous"><?php next_posts_link( '<span class="meta-nav">&larr;</span> По-стари постове' ); ?></div>
+				<div class="nav-next"><?php previous_posts_link( 'По-нови постове <span class="meta-nav">&rarr;</span>' ); ?></div>
 			</div>
 		<?php endif; ?>
 
